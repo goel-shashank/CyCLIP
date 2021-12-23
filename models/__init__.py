9468a0e131a271389fd12b5eff76395e2aa6533d
Shashank Goel <shashank@g10.hoffman2.idre.ucla.edu>
2021-12-22 23:13:41 -0800

    removed garbage files

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .text_encoder import *
-from .image_encoder import *
-from .multimodal_model import *
-from .multimodal_loss import *
--- utils/utils.py
+++
@@ -1,3 +0,0 @@
-import os
-
-root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
