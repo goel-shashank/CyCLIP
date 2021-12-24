89b65a30976519336f2c671f4e41b051e8bc63c4
Shashank Goel <shashank@g11.hoffman2.idre.ucla.edu>
2021-12-24 09:52:33 -0800

    fixed error in testing

--- src/trainer/evaluate.py
+++ src/trainer/evaluate.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from tqdm import tqdm    
 
 def get_validation_metrics(model, dataloader, options):
-    logging.info("Starting validation")
+    logging.info("Started Validating")
 
     metrics = {}
 
@@ -29,24 +29,26 @@ def get_validation_metrics(model, dataloader, options):
         loss = sum(losses) / dataloader.num_samples
         metrics["loss"] = loss
 
-    logging.info("Finished validation")
+    logging.info("Finished Validating")
 
     return metrics
 
 def get_test_metrics(model, processor, dataloader, options):
-    logging.info("Starting test")
+    logging.info("Started Testing")
 
     model.eval()
+    umodel = model.module if(options.distributed) else model
 
     # Generating the class embeddings using multiple templates
-    config = eval(open(options.test_config, "r").read())
+    config = eval(open(f"{options.test_data_dir}/classes.py", "r").read())
     classes, templates = config["classes"], config["templates"]
     with torch.no_grad():
         text_embeddings = []
         for c in tqdm(classes):
-            texts = [template(c) for template in templates]
-            text_tokens = processor.tokenizer(texts, return_tensors = "pt").to(options.map_location)
-            text_embedding = model.module.encode_text(text_tokens) if(options.distributed) else model.encode_text(text_tokens)
+            text = [template(c) for template in templates]
+            text_tokens = processor.text(text)
+            text_input_ids, text_attention_mask = text_tokens["input_ids"].to(options.map_location), text_tokens["attention_mask"].to(options.map_location) 
+            text_embedding = umodel.get_text_features(input_ids = text_input_ids, attention_mask = text_attention_mask)
             text_embedding /= text_embedding.norm(dim = -1, keepdim = True)
             text_embedding = text_embedding.mean(dim = 0)
             text_embedding /= text_embedding.norm()
@@ -58,18 +60,23 @@ def get_test_metrics(model, processor, dataloader, options):
         topk = [1, 3, 5, 10]
         correct = {k: 0 for k in topk}
 
-        for images, labels in tqdm(dataloader):
-            images, labels = images.to(options.map_location), labels.to(options.map_location)
-            image_embeddings = model.module.encode_image(images) if(options.distributed) else model.encode_image(images)
-            
-            logits = (image_embeddings @ text_embeddings)
-            predictions = logits.topk(max(topk), 1)[1].T == labels
+        for image, label in tqdm(dataloader):
+            image, label = image.to(options.map_location), label.to(options.map_location)
+            image_embedding = umodel.get_image_features(image)
+
+            logits = (image_embedding @ text_embeddings)
+            ranks = logits.topk(max(topk), 1)[1].T
+
+            if(len(label.shape) == 1):
+                predictions = ranks == label
+            else:
+                predictions = torch.vstack([torch.any(e == label.T, dim = 0) for e in ranks])
 
             for k in topk:
-                correct[k] += predictions[:k].sum().item() 
+                correct[k] += torch.sum(torch.any(predictions[:k], dim = 0)).item() 
 
-    results = {"zero_shot_top{k}": correct[k] / dataloader.num_samples for k in topk}
-    logging.info("Finished test")
+    results = {f"top{k}": correct[k] / dataloader.num_samples for k in topk}
+    logging.info("Finished Testing")
 
     return results
 
@@ -81,7 +88,11 @@ def evaluate(epoch, model, processor, data, options):
         if(data["test"] is not None): metrics.update(get_test_metrics(model, processor, data["test"], options))
 
         if(metrics):
-            logging.info(f"Epoch {epoch} evaluation")
+            if(epoch == 0):
+                logging.info(f"Base evaluation")
+            else:
+                logging.info(f"Epoch {epoch} evaluation")
+
             for key, value in metrics.items():
                 logging.info(f"{key}: {value:.4f}")
 
