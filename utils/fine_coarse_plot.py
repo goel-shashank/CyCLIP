90e662fe4c6236009175c7500ef1a71e52efce31
Shashank Goel <shashank@g11.hoffman2.idre.ucla.edu>
2022-05-16 16:14:59 -0700

    added utilities

---
+++ utils/fine_coarse_plot.py
@@ -0,0 +1,92 @@
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+params = {
+    "axes.titlesize": 22,
+    "legend.fontsize": 16,
+    "figure.figsize": (12, 8),
+    "axes.labelsize": 16,
+    "axes.titlesize": 16,
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+    "figure.titlesize": 22,
+    "font.family": "Liberation Mono"
+}
+
+plt.rcParams.update(params)
+plt.style.use("seaborn-whitegrid")
+sns.set_style("white")
+
+k = 1
+
+data_fine_top_1 = {
+    "Dataset": ["CIFAR100", "ImageNet1K", "ImageNetSketch", "ImageNetV2"],
+    "CLIP": [43.55,	34.18, 25.11, 31.11],
+    "CyCLIP": [47.11, 35.46, 26.42, 33.01],
+}
+
+data_coarse_top_1 = {
+    "Dataset": ["CIFAR100", "ImageNet1K", "ImageNetSketch", "ImageNetV2"],
+    "CLIP": [34.82, 52.11, 37.03, 47.24],
+    "CyCLIP": [40.37, 56.79, 41.57, 52.15],
+}
+
+data_fine_top_2 = {
+    "Dataset": ["CIFAR100", "ImageNet1K", "ImageNetSketch", "ImageNetV2"],
+    "CLIP": [67.65, 46.53, 36.69, 43.85],
+    "CyCLIP": [68.67, 48.22, 38.34, 45.37],
+}
+
+data_coarse_top_2 = {
+    "Dataset": ["CIFAR100", "ImageNet1K", "ImageNetSketch", "ImageNetV2"],
+    "CLIP": [49.51, 66.06, 49.93, 61.42],
+    "CyCLIP": [56.26, 69.66, 54.59, 65.27],
+}
+
+data_fine_top_3 = {
+    "Dataset": ["CIFAR100", "ImageNet1K", "ImageNetSketch", "ImageNetV2"],
+    "CLIP": [82.41, 53.95, 44.66, 51.61],
+    "CyCLIP": [83.32, 55.43, 46.09, 52.85],
+}
+
+data_coarse_top_3 = {
+    "Dataset": ["CIFAR100", "ImageNet1K", "ImageNetSketch", "ImageNetV2"],
+    "CLIP": [58.39, 73.20, 57.61, 69.09],
+    "CyCLIP": [65.65, 76.45, 62.21, 72.39],
+}
+
+data = (eval(f"data_fine_top_{k}"), eval(f"data_coarse_top_{k}"))
+
+figure, axes = plt.subplots(1, 2, figsize = (15, 5))
+
+X_axis = np.arange(1, 5)
+Y_axis = np.array([0, 10, 20, 30, 40, 50, 60])
+
+for index in range(2):
+    if(index == 0):
+        yaxis_label = "Fine-grained Top1 Accuracy (%)"
+        xaxis_label = "(a)"
+    else:
+        yaxis_label = "Coarse-grained Top1 Accuracy (%)"
+        xaxis_label = "(b)"
+
+    axes[index].set_ylim(0, 70)
+    axes[index].set_xlabel(xaxis_label)
+    axes[index].set_ylabel(yaxis_label)
+    axes[index].set_xticks(X_axis)
+    axes[index].set_yticks(Y_axis)
+    axes[index].set_xticklabels(data[index]["Dataset"], fontsize = 12, rotation = 0)
+    axes[index].set_yticklabels(Y_axis, fontsize = 12, rotation = 0)
+    axes[index].bar(X_axis, data[index]["CLIP"], label = "CLIP", width = 0.3, color = "brown")
+    axes[index].bar(X_axis + 0.3, data[index]["CyCLIP"], width = 0.3, label = "CyCLIP")
+    
+    for i, v in enumerate(data[index]["CLIP"]):
+        axes[index].text(i + 0.81, v + 1, v, fontsize = 11)
+        axes[index].text(i + 1.15, data[index]["CyCLIP"][i] + 1, str(data[index]["CyCLIP"][i]), fontsize = 11)
+
+    axes[index].legend(prop = {"size": 12})
+
+os.makedirs("analysis/plots", exist_ok = True)
+plt.savefig(f"analysis/plots/fine_coarse_top{k}.png")
---
+++ utils/zeroshot.py
@@ -0,0 +1,35 @@
+import torch
+import pickle
+import argparse
+from tqdm import tqdm
+
+def run(options):
+    with torch.no_grad():
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        data = pickle.load(open(options.embeddings, "rb"))
+        image_embeddings, text_embeddings, labels = torch.tensor(data["image_embeddings"]), torch.tensor(data["text_embeddings"]).to(device), torch.tensor(data["labels"])  
+                
+        dataset = torch.utils.data.TensorDataset(image_embeddings, labels)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size = options.batch_size)
+        
+        correct = {k: 0 for k in options.k}
+                
+        for image_embedding, label in tqdm(dataloader):
+            image_embedding, label = image_embedding.to(device), label.to(device)
+            logits = (image_embedding @ text_embeddings.t())
+            ranks = logits.topk(max(options.k), 1)[1].T
+            predictions = ranks == label
+            for k in options.k:
+                correct[k] += torch.sum(torch.any(predictions[:k], dim = 0)).item() 
+
+        for k in options.k:
+            print(f"Zeroshot top {k:2d}: {correct[k] / len(dataset) * 100.0}")    
+        
+if(__name__ == "__main__"):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--embeddings", type = str, default = "analysis/embeddings/clip/CIFAR10.test.pkl", help = "Input test embeddings file")
+    parser.add_argument("--batch_size", type = int, default = 32, help = "Batch size")
+    parser.add_argument("--k", nargs = "+", default = [1, 3, 5, 10])
+    options = parser.parse_args()
+    run(options)
