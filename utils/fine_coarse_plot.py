97d8cccc9b4f117d82823ac34aabdb72cb663145
Shashank Goel <shashank@login4.hoffman2.idre.ucla.edu>
2022-05-23 15:26:14 -0700

    minor changes

--- utils/fine_coarse_plot.py
+++ utils/fine_coarse_plot.py
@@ -25,38 +25,52 @@ k = 1
 data_fine_top_1 = {
     "Dataset": ["CIFAR-100", "ImageNet1K", "ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"],
     "CLIP": [43.55,	34.18, 31.10, 25.11, 40.09, 54.49],
-    "CyCLIP": [47.11, 35.46, 33.01, 26.42, 41.25, 55.36],
+    "CyCLIP": [47.11, 35.46, 33.01, 26.42, 41.25, 55.36],    
+    "C-CyCLIP": [49.19, 35.12, 32.31, 26.37, 42.56, 55.59],
+    "I-CyCLIP": [47.71, 34.92, 32.21, 24.89, 39.60, 53.99]
 }
 
 data_coarse_top_1 = {
     "Dataset": ["CIFAR-100", "ImageNet1K", "ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"],
     "CLIP": [34.82, 52.11, 47.24, 37.03, 14.59, 40.51],
-    "CyCLIP": [40.37, 56.79, 52.15, 41.57, 16.48, 44.43],
+    "CyCLIP": [40.37, 56.79, 52.15, 41.57, 16.48, 44.43],    
+    "C-CyCLIP": [43.06, 56.08, 50.89, 43.02, 16.40, 45.41],
+    "I-CyCLIP": [39.30, 55.78, 51.48, 39.18, 15.63, 41.63]
 }
 
 data_fine_top_2 = {
     "Dataset": ["CIFAR-100", "ImageNet1K", "ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"],
     "CLIP": [67.65, 46.53, 43.85, 36.69, 57.93, 71.55],
-    "CyCLIP": [68.67, 48.22, 45.37, 38.34, 57.89, 71.96],
+    "CyCLIP": [68.67, 48.22, 45.37, 38.34, 57.89, 71.96],    
+    "C-CyCLIP": [71.02, 47.95, 45.02, 37.67, 59.41, 72.06],
+    "I-CyCLIP": [69.30, 47.42, 45.00, 37.24, 56.67, 70.99]
 }
 
 data_coarse_top_2 = {
     "Dataset": ["CIFAR-100", "ImageNet1K", "ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"],
     "CLIP": [49.51, 66.06, 61.42, 49.93, 24.71, 52.77],
-    "CyCLIP": [56.26, 69.67, 65.27, 54.59, 26.75, 55.98],
+    "CyCLIP": [56.26, 69.67, 65.27, 54.59, 26.75, 55.98],    
+    "C-CyCLIP": [58.62, 69.74, 65.23, 56.41, 26.76, 57.21],
+    "I-CyCLIP": [54.20, 68.46, 63.85, 51.58, 25.72, 51.94]
 }
 
 data_fine_top_3 = {
     "Dataset": ["CIFAR-100", "ImageNet1K", "ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"],
     "CLIP": [82.41, 53.95, 51.61, 44.66, 67.80, 79.52],
-    "CyCLIP": [83.32, 55.43, 52.85, 46.09, 68.75, 79.81],
+    "CyCLIP": [83.32, 55.43, 52.85, 46.09, 68.75, 79.81],    
+    "C-CyCLIP": [85.26, 55.14, 52.40, 45.41, 69.99, 79.60],
+    "I-CyCLIP": [84.07, 54.95, 52.03, 45.30, 67.92, 79.41]
 }
 
 data_coarse_top_3 = {
     "Dataset": ["CIFAR-100", "ImageNet1K", "ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"],
     "CLIP": [58.39, 73.20, 69.09, 57.61, 32.49, 60.07],
-    "CyCLIP": [65.65, 76.45, 72.40, 62.22, 34.83, 63.12],
+    "CyCLIP": [65.65, 76.45, 72.40, 62.22, 34.83, 63.12],    
+    "C-CyCLIP": [67.91, 76.76, 72.55, 64.11, 35.25, 64.3],
+    "I-CyCLIP": [63.30, 74.98, 70.44, 58.68, 32.76, 58.48]
 }
+
+model = "CyCLIP"
 
 ylims = {1: 80, 2: 100, 3: 100}
 for k in [1, 2, 3]:
@@ -78,10 +92,10 @@ for k in [1, 2, 3]:
         axes[index].set_xticklabels(data[index]["Dataset"], fontsize = 12, rotation = 30)
         axes[index].set_yticklabels(Y_axis, fontsize = 12, rotation = 0)
         axes[index].bar(X_axis, data[index]["CLIP"], label = "CLIP", width = 0.35, color = "brown", alpha = 0.85)
-        axes[index].bar(X_axis + 0.37, data[index]["CyCLIP"], width = 0.35, label = "CyCLIP", alpha = 0.85)
+        axes[index].bar(X_axis + 0.37, data[index][model], width = 0.35, label = model, alpha = 0.85)
         
         for i, v in enumerate(data[index]["CLIP"]):
-            axes[index].text(i + 0.9, data[index]["CyCLIP"][i] + 1.5, "+" + str(round(data[index]["CyCLIP"][i] - data[index]["CLIP"][i], 2)) + "%", fontsize = 12)
+            axes[index].text(i + 0.9, data[index][model][i] + 1.5, "+" + str(round(data[index][model][i] - data[index]["CLIP"][i], 2)) + "%", fontsize = 12)
 
         axes[index].legend(prop = {"size": 12})
         
@@ -89,4 +103,4 @@ for k in [1, 2, 3]:
     plt.subplots_adjust(wspace = 0.225)
 
     os.makedirs("analysis/plots", exist_ok = True)
-    plt.savefig(f"analysis/plots/fine_coarse_top{k}.png")
+    plt.savefig(f"analysis/plots/fine_coarse_top{k}.{model}.png")
