d5d9a00410ba4f1c5e0e8afa0d0b748476c3049a
Shashank Goel <shashank@g6.hoffman2.idre.ucla.edu>
2022-05-23 20:20:26 -0700

    minor bugs and fixes

--- utils/effective_robustness_plot.py
+++ utils/effective_robustness_plot.py
@@ -34,41 +34,35 @@ cyclip = {
     "ImageNet-R": [6.80, 13.45, 19.64, 22.45]
 }
 
-for dataset in ["ImageNetV2", "ImageNetSketch", "ImageNet-A", "ImageNet-R"]:
-    plt.clf()
+plt.clf()
     
-    plt.scatter(clip["ImageNet1K"][0], clip[dataset][0], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][0]}")
-    plt.scatter(clip["ImageNet1K"][1], clip[dataset][1], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][1]}")
-    plt.scatter(clip["ImageNet1K"][2], clip[dataset][2], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][2]}")
-    plt.scatter(clip["ImageNet1K"][3], clip[dataset][3], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][3]}")
+plt.scatter(clip["ImageNet1K"][0], clip["ImageNetV2"][0], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][0]}")
+plt.scatter(clip["ImageNet1K"][1], clip["ImageNetV2"][1], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][1]}")
+plt.scatter(clip["ImageNet1K"][2], clip["ImageNetV2"][2], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][2]}")
+plt.scatter(clip["ImageNet1K"][3], clip["ImageNetV2"][3], marker = "^", s = 80, label = f"CLIP-{clip['Datasize'][3]}")
 
-    plt.scatter(cyclip["ImageNet1K"][0], cyclip[dataset][0], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][0]}")
-    plt.scatter(cyclip["ImageNet1K"][1], cyclip[dataset][1], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][1]}")
-    plt.scatter(cyclip["ImageNet1K"][2], cyclip[dataset][2], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][2]}")
-    plt.scatter(cyclip["ImageNet1K"][3], cyclip[dataset][3], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][3]}")
+plt.scatter(cyclip["ImageNet1K"][0], cyclip["ImageNetV2"][0], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][0]}")
+plt.scatter(cyclip["ImageNet1K"][1], cyclip["ImageNetV2"][1], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][1]}")
+plt.scatter(cyclip["ImageNet1K"][2], cyclip["ImageNetV2"][2], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][2]}")
+plt.scatter(cyclip["ImageNet1K"][3], cyclip["ImageNetV2"][3], marker = "*", s = 96, label = f"CyCLIP-{cyclip['Datasize'][3]}")
 
-    plt.xlabel("Top1 Accuracy on ImageNet1K (%)", labelpad = 12)
-    plt.ylabel(f"Top1 Accuracy on {dataset} (%)", labelpad = 12)
-    xpoints = ypoints = (0, 25)
-    plt.plot(xpoints, ypoints, linestyle = "--", color = "k", lw = 2, scalex = False, scaley = False, label = "y = x")
-    
-    ypoints = (0, 21)
-    xpoints = [(y + 9) / 1.2 for y in ypoints]
-    plt.plot(xpoints, ypoints, linestyle = "-", color = "r", lw = 1, label = "Linear fit to\nstandard training")
+plt.xlabel("Top1 Accuracy on ImageNet1K (%)", labelpad = 12)
+plt.ylabel(f"Top1 Accuracy on ImageNetV2 (%)", labelpad = 12)
+xpoints = ypoints = plt.xlim()
+plt.plot(xpoints, ypoints, linestyle = "--", color = "k", lw = 2, scalex = False, scaley = False, label = "y = x")
 
-    # plt.yticks(np.arange(5.0, 22.5, 2.5))
-    # plt.xticks(np.arange(5.0, 25.0, 2.5))
-    # plt.ylim(2.5, 20.0)
-    # plt.xlim(2.5, 22.5)
+ypoints = plt.ylim()
+xpoints = [(y + 9) / 1.2 for y in ypoints]
+plt.plot(xpoints, ypoints, linestyle = "-", color = "r", lw = 1, label = "Linear fit to\nstandard training")
+
+plt.yticks(np.arange(5.0, 22.5, 2.5))
+plt.xticks(np.arange(5.0, 25.0, 2.5))
+plt.ylim(2.5, 20.0)
+plt.xlim(2.5, 22.5)
     
-    plt.yticks(np.arange(2.5, 25.0, 2.5))
-    plt.xticks(np.arange(2.5, 25.0, 2.5))
-    plt.ylim(0, 25.0)
-    plt.xlim(0, 25.0)
-        
-    plt.legend(bbox_to_anchor = (1.0, 1.05))
-    plt.grid()
-    plt.tight_layout()
-    
-    os.makedirs("analysis/plots", exist_ok = True)
-    plt.savefig(f"analysis/plots/effective_robustness_plot.{dataset}.png")
+plt.legend(bbox_to_anchor = (1.0, 1.05))
+plt.grid()
+plt.tight_layout()
+
+os.makedirs("analysis/plots", exist_ok = True)
+plt.savefig(f"analysis/plots/effective_robustness_plot.png")
